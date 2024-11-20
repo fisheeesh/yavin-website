edd5c7e98182f3cf41c9cf1a40c2fd6c0b2fcd30
fisheeesh <swanphyo444@gmail.com>
2024-11-20 17:20:16 +0700

    navigation & header

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function userScoll(){
+    const navbar = document.querySelector('.navbar')
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50) navbar.classList.add('navbar-sticky')
+        else navbar.classList.remove('navbar-sticky')
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', userScoll)
